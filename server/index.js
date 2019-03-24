6f056ce7dee0d1b920a13f0585355d5633824cab
Ricardo Vogel <info@ricardovogel.nl>
2019-03-24 19:12:48 +0100

    Remove old comments

--- server/index.js
+++ server/index.js
@@ -8,14 +8,6 @@ const app = express();
 const server = http.createServer(app);
 app.use(express.static(`client`));
 
-// app.set("view-engine", "ejs");
-// app.set('views', __dirname.replace("server", "client/views"));
-
-
-// app.get("/", function (req, res) {
-//     res.sendFile('index.html');
-// });
-
 logging.logDebug("server starting");
 server.listen(process.env.PORT || 3000);
 logging.logDebug("server started");
