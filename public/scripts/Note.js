69cd4d0d521b2508ea1a2926d9a3af279d01ed9f
Ricardo Vogel <info@ricardovogel.nl>
2019-04-22 10:57:19 +0200

    Delete Notes

--- public/scripts/Note.js
+++ public/scripts/Note.js
@@ -1,9 +1,17 @@
 class Note {
-    constructor(id, text) {
+    constructor(id, text, style, data_x, data_y) {
         this.id = id;
         this.text = text;
-        this.style = `transform: translate(5px, -5px);`;
-        this.data_x = "5px";
-        this.data_y = "-5px";
+        this.style = style || `transform: translate(5px, -5px);`;
+        this.data_x = data_x || "5px";
+        this.data_y = data_y || "-5px";
     }
+}
+
+function makeNullNode(note) {
+    note.style = "display:none;";
+    delete note.id;
+    delete note.text;
+    delete note.data_x;
+    delete note.data_y;
 }
