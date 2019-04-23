fcc34af3000c0eb8d5afd9bd225a74fb7f811d69
Ricardo Vogel <info@ricardovogel.nl>
2019-04-23 18:50:12 +0200

    Add nicer popups

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -38,46 +38,93 @@ function addImgNote() {
     app.imgnotes.push(noteToAdd);
 }
 
-function edittext(noteNumber) {
-    console.log(noteNumber)
-    noteNumber = noteNumber.split("-")[1];
-    console.log(noteNumber)
-    let text = prompt("Enter your text", app.notes[noteNumber].text);
-    app.notes[noteNumber].text = text;
-}
-
-
-function deleteImgNote(noteNumber) {
-    if (confirm("Are you sure you want to delete this note? This action cannot be undone.")) {
-        deleteImgNoteConfirmed(noteNumber);
-    }
-}
-
-function deleteImgNoteConfirmed(noteNumber) {
-    noteNumber = noteNumber.split("-")[1];
-    makeNullNode(app.imgnotes[noteNumber]);
-    storage.ref().child(`board_pictures/${app.user.uid}/${noteNumber}`).delete().then(function () {
-    }).catch(function (error) {
-        console.error(error);
+function edittext(noteID) {
+    let noteNumber = noteID.split("-")[1];
+    Swal.fire({
+        title: 'Change text',
+        html: `<input id="text-to-change-to" type="text" value="${app.notes[noteNumber].text}" />`,
+        type: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Change'
+    }).then((result) => {
+        if (result.value) {
+            app.notes[noteNumber].text = document.getElementById("text-to-change-to").value;
+        }
     });
-
 }
 
-function deleteNote(noteNumber) {
-    if (confirm("Are you sure you want to delete this note? This action cannot be undone.")) {
-        deleteNoteConfirmed(noteNumber);
-    }
+
+function deleteImgNote(noteID) {
+    Swal.fire({
+        title: 'Are you sure?',
+        html: `Are you sure you want to delete this note? This action cannot be undone. Your crimeboard will be saved after the deletion.`,
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it'
+    }).then((result) => {
+        if (result.value) {
+            let noteNumber = noteID.split("-")[1];
+            makeNullNode(app.imgnotes[noteNumber]);
+            storage.ref().child(`board_pictures/${app.user.uid}/${noteNumber}`).delete().then(function () {
+                Swal.fire(
+                    'Picture removed',
+                    'This picture has been removed successfully',
+                    'success'
+                );
+                saveState(true);
+            }).catch(function (error) {
+                if (JSON.parse(error.serverResponse_).error.code != "404") {
+                    console.error(error);
+                    Swal.fire(
+                        'Something went wrong...',
+                        'Something went wrong in trying to delete the picture. You might see old pictures showing up. Try again later, or contact the team.',
+                        'error'
+                    );
+                }
+            });
+        }
+    });
 }
 
-function deleteNoteConfirmed(noteNumber) {
-    noteNumber = noteNumber.split("-")[1];
-    makeNullNode(app.notes[noteNumber]);
+function deleteNote(noteID) {
+    Swal.fire({
+        title: 'Are you sure?',
+        html: `Are you sure you want to delete this note? This action cannot be undone`,
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, delete it'
+    }).then((result) => {
+        if (result.value) {
+            let noteNumber = noteID.split("-")[1];
+            makeNullNode(app.notes[noteNumber]);
+            Swal.fire(
+                'Note removed',
+                'This note has been removed successfully',
+                'success'
+            );
+        }
+    });
 }
 
 function clearBoard() {
-    if (confirm("Are you sure you want to clear your board? This action cannot be undone.")) {
-        clearBoardConfirmed();
-    }
+    Swal.fire({
+        title: 'Are you sure?',
+        html: `Are you sure you want to clear your crimeboard? This action cannot be undone`,
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Yes, clear it'
+    }).then((result) => {
+        if (result.value) {
+            clearBoardConfirmed();
+        }
+    });
 }
 
 function clearBoardConfirmed() {
@@ -86,16 +133,29 @@ function clearBoardConfirmed() {
         storage.ref().child(`board_pictures/${app.user.uid}/${i}`).delete().then((function () { }))
             .catch(function (error) {
                 console.error(error);
+                if (JSON.parse(error.serverResponse_).error.code != "404") {
+                    Swal.fire(
+                        'Something went wrong...',
+                        'Something went wrong in trying to clear the pictures. You might see old pictures showing up. Try again later, or contact the team.',
+                        'error'
+                    );
+                }
             });
     }
     app.imgnotes = [];
+    Swal.fire(
+        'Board cleared',
+        'Your crimeboard has been cleared successfully',
+        'success'
+    );
+    saveState(true);
 }
 
-function uploadImage(noteId) {
-    noteId = noteId.split("-")[1] || noteId;
+function uploadImage(noteID) {
+    let noteNumber = noteID.split("-")[1] || noteID;
     Swal.fire({
-        title: 'Upload file',
-        html: `<input id="image-file" type="file" />`,
+        title: 'Upload picture',
+        html: `Your crimeboard will be saved after uploading.<input id="image-file" type="file" >`,
         type: 'question',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
@@ -103,7 +163,7 @@ function uploadImage(noteId) {
         confirmButtonText: 'Upload'
     }).then((result) => {
         if (result.value) {
-            let storageRef = storage.ref().child(`board_pictures/${app.user.uid}/${noteId}`);
+            let storageRef = storage.ref().child(`board_pictures/${app.user.uid}/${noteNumber}`);
             let file = document.getElementById("image-file").files[0];
             if (!file) {
                 Swal.fire(
@@ -111,22 +171,23 @@ function uploadImage(noteId) {
                     'Please select a file.',
                     'warning'
                 ).then(() => {
-                    uploadImage(noteId);
+                    uploadImage(noteNumber);
                     return;
                 });
             }
             storageRef.put(file).then(function (snapshot) {
                 storageRef.getDownloadURL().then(function (url) {
-                    app.imgnotes[noteId].style = app.imgnotes[noteId].style.replace(/url\(.*?\)/, `url("${url}")`);
+                    app.imgnotes[noteNumber].style = app.imgnotes[noteNumber].style.replace(/url\(.*?\)/, `url("${url}")`);
+
+                    Swal.fire(
+                        'Uploaded!',
+                        'Your file has been uploaded.',
+                        'success'
+                    );
+                    saveState(true);
                 }).catch((error) => {
                     console.error(error);
                 });
-
-                Swal.fire(
-                    'Uploaded!',
-                    'Your file has been uploaded.',
-                    'success'
-                );
             }).catch(function (error) {
                 Swal.fire(
                     'Upload failed',
--- public/scripts/firebaseApp.js
+++ public/scripts/firebaseApp.js
@@ -17,14 +17,25 @@ window.onload = function () {
 };
 
 
-function saveState() {
+function saveState(dontTrigger) {
     db.collection("userNotes").doc(app.user.uid).set({
         notes: JSON.stringify(app.notes),
         imgnotes: JSON.stringify(app.imgnotes)
     }).then(function () {
-        console.log(":D")
+        if (!dontTrigger) {
+            Swal.fire(
+                'Saved!',
+                'Your data has been saved successfully',
+                'success'
+            );
+        }
     }).catch(function () {
         console.error("Error adding document: ", error);
+        Swal.fire(
+            'Something went wrong...',
+            'Please try again later',
+            'error'
+        );
     });
 }
 
